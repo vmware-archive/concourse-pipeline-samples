29e763cf15ad7c55dc5020db069c9b6c50597167
Brandon Blincoe <bblincoe@gmail.com>
2017-03-07 17:13:14 -0600

    Clean up backup files

--- pcf-cfops-backup/scripts/cfops_s3backup.sh
+++ pcf-cfops-backup/scripts/cfops_s3backup.sh
@@ -101,6 +101,8 @@ cd $BACKUP_ROOT_DIR
 #   aws --debug s3 mv . s3://${S3_BUCKET} --recursive
 # fi
 
+ls -alR
+
 # cleanup backup file from container to minimize worker disk size usage
 cd $BACKUP_ROOT_DIR
 rm -R *
