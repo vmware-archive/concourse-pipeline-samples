dad421ad212976d27e9caaebf1a207183dbc4eaa
Luciano Silva <lsilva@pivotal.io>
2018-02-27 16:57:42 -0500

    Add docker image creation sample

---
+++ pipelines/docker/pks-kubectl-image/README.md
@@ -0,0 +1,5 @@
+![Concourse and a Private Docker Registry](https://raw.githubusercontent.com/pivotalservices/concourse-pipeline-samples/master/common/images/concourse-and-private-registry.jpg)
+
+# Pipeline for creating or updating a Docker image in Docker Hub
+
+TBD
---
+++ pipelines/docker/pks-kubectl-image/pks_kubectl_image_params.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# set the path for the secrets below to be created in vault or credhub
+export concourse_root_secrets_path="/concourse"
+export concourse_team_name="team-name"
+export concourse_pipeline_name="pks-kubectl-image"
+
+# VAULT or CREDHUB - targeted secrets management system
+export targeted_system="VAULT"
+# This script assumes that:
+# 1) the credhub or vault CLI is installed
+# 2) you setup your vault or credhub target and login commands prior to invoking it
+#    e.g. for VAULT
+#    export VAULT_ADDR=https://myvaultdomain:8200
+#    export VAULT_SKIP_VERIFY=true
+#    export VAULT_TOKEN=vault-token
+#
+#    e.g. for CREDHUB
+#    credhub login -s credhub-server-uri -u username -p password --skip-tls-validation
+
+##
+## TEAM level secrets (shared by all pipelines in that team)
+##
+export team_secrets=(
+  "dockerhub-username"::"dockerhub-username"
+  "dockerhub-password"::"dockerhub-password"
+  "pivnet_token"::"pivotal-network-token"
+)
+
+##
+## PIPELINE LEVEL secrets (specific to the pipeline)
+##
+export pipeline_secrets=(
+  # "^1\.0\..*$"
+  "pks_product_version"::"^.*$"
+  "pks_product_slug"::"pivotal-container-service"
+
+)
+
+main () {
+
+  # team level secrets
+  concourse_team_level_secrets_path="${concourse_root_secrets_path}/${concourse_team_name}"
+  writeCredentials "${concourse_team_level_secrets_path}" "${team_secrets[*]}"
+
+  # pipeline level secrets
+  concourse_pipeline_level_secrets_path="${concourse_team_level_secrets_path}/${concourse_pipeline_name}"
+  writeCredentials "${concourse_pipeline_level_secrets_path}" "${pipeline_secrets[*]}"
+
+}
+
+writeCredentials () {
+  secretsPath=${1}
+  secretsObject=(${2})
+
+  for i in "${secretsObject[@]}"
+  do
+    KEY="${i%%::*}"
+    VALUE="${i##*::}"
+    echo "Creating secret for [$KEY]"
+    if [[ $targeted_system == "VAULT" ]]; then
+      vault write "${secretsPath}/${KEY}" value="${VALUE}"
+    else   # CREDHUB
+      credhub set -n "${secretsPath}/${KEY}" -v "${VALUE}"
+    fi
+  done
+}
+
+main
