2e484d5916d5ba4dd52d44267ef657f8db5f808f
Luciano Silva <lsilva@pivotal.io>
2018-03-01 15:28:56 -0500

    Create tasks for PKS create cluster

---
+++ tasks/pcf/create-pks-cluster/task.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eu
+
+echo "Login to PKS API [$PCF_PKS_API]"
+pks login -a "$PCF_PKS_API" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-verification # TBD --ca-cert CERT-PATH
+
+echo "Creating PKS cluster [$PKS_CLUSTER_NAME], master node hostname [$PKS_CLUSTER_MASTER_HOSTNAME], plan [$PKS_SERVICE_PLAN_NAME], number of workers [$PKS_CLUSTER_NUMBER_OF_WORKERS]"
+pks create-cluster "$PKS_CLUSTER_NAME" --external-hostname "$PKS_CLUSTER_MASTER_HOSTNAME" --plan "$PKS_SERVICE_PLAN_NAME" --num-nodes "$PKS_CLUSTER_NUMBER_OF_WORKERS"
+
+echo "Monitoring the creation status for PKS cluster [$PKS_CLUSTER_NAME]:"
+in_progress_state="in progress"
+succeeded_state="succeeded"
+cluster_state="$in_progress_state"
+
+while [[ "$cluster_state" == "$in_progress_state" ]]; do
+  cluster_state=$(pks cluster "$PKS_CLUSTER_NAME" --json | jq -rc '.last_action_state')
+  echo "${cluster_state}..."
+  sleep 5
+done
+
+last_action_description=$(pks cluster "$PKS_CLUSTER_NAME" --json | jq -rc '.last_action_description')
+
+if [[ "$cluster_state" == "$succeeded_state" ]]; then
+  echo "Successfully created cluster [$PKS_CLUSTER_NAME], last_action_state=[$cluster_state], last_action_description=[$last_action_description]"
+  pks cluster "$PKS_CLUSTER_NAME"
+  echo "Next step: make sure that the external hostname configured for the cluster [$PKS_CLUSTER_MASTER_HOSTNAME] is accessible from a DNS/network standpoint, so it can be managed with 'kubectl'"
+else
+  echo "Error creating cluster [$PKS_CLUSTER_NAME], last_action_state=[$cluster_state], last_action_description=[$last_action_description]"
+  exit 1
+fi
---
+++ tasks/pcf/delete-pks-cluster/task.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+echo "Login to PKS API [$PCF_PKS_API]"
+pks login -a "$PCF_PKS_API" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-verification # TBD --ca-cert CERT-PATH
+pks cluster "$PKS_CLUSTER_NAME"
+
+echo "Deleting PKS cluster [$PKS_CLUSTER_NAME]..."
+pks delete-cluster "$PKS_CLUSTER_NAME"
+
+echo "Monitoring the deletion status for PKS cluster [$PKS_CLUSTER_NAME]"
+in_progress_state="in progress"
+cluster_state="$in_progress_state"
+
+while [[ "$cluster_state" == "$in_progress_state" ]]; do
+  cluster_state=$(pks cluster "$PKS_CLUSTER_NAME" --json | jq -rc '.last_action_state')
+  echo "status: [$cluster_state]..."
+  sleep 5
+done
+
+cluster_exists=$(pks clusters --json | jq -rc '.[].name')
+
+if [[ "$cluster_exists" == "" ]]; then
+  echo "Successfully deleted cluster [$PKS_CLUSTER_NAME]"
+  echo "Current list of PKS clusters:"
+  pks clusters --json
+else
+  last_action_description=$(pks cluster "$PKS_CLUSTER_NAME" --json | jq -rc '.last_action_description')
+  echo "Error deleting cluster [$PKS_CLUSTER_NAME], last_action_state=[$cluster_state], last_action_description=[$last_action_description]"
+  exit 1
+fi
