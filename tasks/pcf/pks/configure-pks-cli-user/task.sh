53fc0451b387c12eecbe4dffaed8ef40dfad47b2
Luciano Silva <lsilva@pivotal.io>
2018-06-06 10:26:36 -0400

    Update pks api uaa client setup task

--- tasks/pcf/pks/configure-pks-cli-user/task.sh
+++ tasks/pcf/pks/configure-pks-cli-user/task.sh
@@ -2,7 +2,7 @@
 set -eu
 
 echo "Note - pre-requisite for this task to work:"
-echo "- Your PKS API endpoint [api.$PKS_DOMAIN] should be routable and accessible from the Concourse worker(s) network."
+echo "- Your PKS API endpoint [$PKS_API_DOMAIN] should be routable and accessible from the Concourse worker(s) network."
 echo "- See PKS tile documentation for configuration details for vSphere [https://docs.pivotal.io/runtimes/pks/1-0/installing-pks-vsphere.html#loadbalancer-pks-api] and GCP [https://docs.pivotal.io/runtimes/pks/1-0/installing-pks-gcp.html#loadbalancer-pks-api]"
 
 echo "Retrieving PKS tile properties from Ops Manager [https://$OPSMAN_DOMAIN_OR_IP_ADDRESS]..."
@@ -11,9 +11,9 @@ PRODUCTS=$(om-linux --target "https://$OPSMAN_DOMAIN_OR_IP_ADDRESS" --client-id 
 PKS_GUID=$(echo "$PRODUCTS" | jq -r '.[] | .guid' | grep pivotal-container-service)
 UAA_ADMIN_SECRET=$(om-linux --target "https://$OPSMAN_DOMAIN_OR_IP_ADDRESS" --client-id "${OPSMAN_CLIENT_ID}" --client-secret "${OPSMAN_CLIENT_SECRET}" --username "$OPSMAN_USERNAME" --password "$OPSMAN_PASSWORD" --skip-ssl-validation curl -p /api/v0/deployed/products/$PKS_GUID/credentials/.properties.uaa_admin_secret | jq -rc '.credential.value.secret')
 
-echo "Connecting to PKS UAA server [api.<$PKS_DOMAIN>]..."
+echo "Connecting to PKS UAA server [<$PKS_API_DOMAIN>]..."
 # login to PKS UAA
-uaac target https://api.$PKS_DOMAIN:8443 --skip-ssl-validation
+uaac target https://$PKS_API_DOMAIN:8443 --skip-ssl-validation
 uaac token client get admin --secret $UAA_ADMIN_SECRET
 
 echo "Creating PKS CLI administrator user per PK tile documentation https://docs.pivotal.io/runtimes/pks/1-0/manage-users.html#uaa-scopes"
@@ -25,4 +25,4 @@ echo "PKS CLI administrator user [$PKS_CLI_USERNAME] successfully created."
 
 echo "Next, download the PKS CLI from Pivotal Network and login to the PKS API to create a new K8s cluster [https://docs.pivotal.io/runtimes/pks/1-0/create-cluster.html]"
 echo "Example: "
-echo "   pks login -a api.$PKS_DOMAIN -u $PKS_CLI_USERNAME -p <pks-cli-password-provided>"
+echo "   pks login -a $PKS_API_DOMAIN -u $PKS_CLI_USERNAME -p <pks-cli-password-provided>"
