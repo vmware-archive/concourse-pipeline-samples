7801d0a90ea5582eee65d842cac79e842e18b291
MinSeok Kim <myminseok@gmail.com>
2018-11-15 23:14:22 +0900

    fix for PKS 1.2 error "release not found for version:" Ubuntu Xenial

--- tasks/pcf/upload-product-and-stemcell/task.sh
+++ tasks/pcf/upload-product-and-stemcell/task.sh
@@ -51,6 +51,8 @@ if [ -n "$STEMCELL_VERSION" ]; then
         '
         if any(.Dependencies[]; select(.Release.Product.Name | contains("Stemcells for PCF (Windows)"))) then
           "stemcells-windows-server"
+        elif any(.Dependencies[]; select(.Release.Product.Name | contains("Stemcells for PCF (Ubuntu Xenial)"))) then
+          "stemcells-ubuntu-xenial"
         else
           "stemcells"
         end
